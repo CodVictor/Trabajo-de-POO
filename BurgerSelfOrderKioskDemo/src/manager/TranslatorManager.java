9074e523d8e3d4b9159e15a21b249ab0e62ba40b
ruben730 <rubenruizmartin.05@gmail.com>
2025-01-14 22:29:32 +0100

    PONGO TODO CON LA ESTRUCTURA QUE DA EL PROFE

---
+++ BurgerSelfOrderKioskDemo/src/manager/TranslatorManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manager;
+import java.util.Map;
+/**
+ *
+ * @author Usuario
+ */
+public class TranslatorManager {
+    private Translator currentDictionary;
+    private Map<String,Translator> dictionaries;
+}
