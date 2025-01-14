157fe929dfee6be1d8a04f76e51c3f23553419e6
ruben730 <rubenruizmartin.05@gmail.com>
2025-01-14 18:44:18 +0100

    añado las interfacess

--- BurgerSelfKioskApp/src/products/Product.java
+++ BurgerSelfKioskApp/src/products/Product.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package products;
 
@@ -8,6 +8,6 @@ package products;
  *
  * @author Usuario
  */
-public class Product {
+public interface Product {
     
 }
