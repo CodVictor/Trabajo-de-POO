ae4ae3c6ee718e29f6eeca2eabb4c3d5fe78eb91
ruben730 <rubenruizmartin.05@gmail.com>
2025-01-14 18:45:37 +0100

    añado las interfaces

--- BurgerSelfKioskApp/src/manager/KioskScreen.java
+++ BurgerSelfKioskApp/src/manager/KioskScreen.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package manager;
 
@@ -8,6 +8,6 @@ package manager;
  *
  * @author Usuario
  */
-public class KioskScreen {
+public interface KioskScreen {
     
 }
