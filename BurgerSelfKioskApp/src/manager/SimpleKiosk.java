95ea405cb28af968101f3496c3af7da595fac5cd
ruben730 <rubenruizmartin.05@gmail.com>
2025-01-14 18:28:33 +0100

    cambio estructura general

--- BurgerSelfKioskApp/src/manager/SimpleKiosk.java
+++ BurgerSelfKioskApp/src/manager/SimpleKiosk.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package menu;
+package manager;
 
 /**
  *
- * @author victor
+ * @author Usuario
  */
-public class Product {
+public class SimpleKiosk {
     
 }
--- BurgerSelfKioskApp/src/manager/TranslatorManager.java
+++ BurgerSelfKioskApp/src/manager/TranslatorManager.java
@@ -2,16 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package utils;
+package manager;
 
 /**
  *
- * @author victor
+ * @author Usuario
  */
 public class TranslatorManager {
-
-    public void loadTranslators() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
     
 }
